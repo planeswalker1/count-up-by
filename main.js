996a65ba66cd43a9310708d357c7520274e2ab9c
Daniel Munoz <daniel.munozdev@gmail.com>
2018-08-19 14:19:26 -0700

    add starter files

---
+++ main.js
@@ -0,0 +1,18 @@
+// Count Up By
+// Create a program that takes two numbers
+// - one to count to and another to determine what multiple to use to get there.
+
+// Here is some sample input:
+// Count by: 5
+// Output: 5, 10, 15, 20, 25, 30
+
+// Count to: 50
+// Count by: 7
+// Output: 7, 14, 21, 28, 35, 42, 49
+
+// Don't forget to consider how to handle any input that might be submitted:
+
+// empty values(user just presses enter)
+// non - numeric values(It may be helpful to read up about the numerical value, NaN)
+// negative numbers
+// count by number is larger than the count to numbers
