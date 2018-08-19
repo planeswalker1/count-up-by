8e5e81cca9d97278a0ba149c192dda6fc6831326
Daniel Munoz <daniel.munozdev@gmail.com>
2018-08-19 16:09:49 -0700

    add form event listener with looping functionality

--- main.js
+++ main.js
@@ -16,3 +16,81 @@
 // non - numeric values(It may be helpful to read up about the numerical value, NaN)
 // negative numbers
 // count by number is larger than the count to numbers
+
+// loop to end with a start and step
+// return an array of looped numbers
+function generateCount(start, end, step) {
+  let numbers = [];
+  if (end <= start) {
+    // if end === start
+    if (end === start) {
+      for (let i = start; i === end; i += step) {
+        numbers.push(i);
+      }
+      // if end < start
+    } else {
+      for (let i = start; i >= end; i += step) {
+        numbers.push(i);
+      }
+    }
+    // if end > start
+  } else {
+    for (let i = start; i <= end; i += step) {
+      numbers.push(i);
+    }
+  }
+
+  return numbers;
+}
+
+// check for errors and prompt error
+// add looped values to outputP
+function stepToCount(count, step) {
+  // check errors
+  if (count === '' || step === '') {
+    outputP.textContent = 'Please enter a value.';
+    return;
+  }
+
+  let countNumber = Number(count);
+  let stepNumber = Number(step);
+
+  if (isNaN(countNumber) || isNaN(stepNumber) || stepNumber > countNumber && stepNumber >= 0 || stepNumber < countNumber && stepNumber <= 0 || stepNumber === countNumber && stepNumber === 0) {
+    outputP.textContent = 'Please Enter valid values';
+    return;
+  }
+
+  // loop count and add values to outputP
+  let numbers = generateCount(stepNumber, countNumber, stepNumber);
+  outputP.textContent = 'Output: ';
+  if (numbers.length > 0) {
+    for (let i = 0; i < numbers.length; i++) {
+      if ((i + 1) === numbers.length) {
+        outputP.textContent += numbers[i];
+      } else {
+        outputP.textContent += numbers[i] + ', ';
+      }
+    }
+  }
+}
+
+const inputs = Array.from(document.querySelectorAll('input'));
+const count = document.querySelector('#count');
+const step = document.querySelector('#step');
+const form = document.querySelector('form');
+const outputP = document.querySelector('.hidden');
+
+// on form submit show outputP with looped values
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  outputP.textContent = '';
+  stepToCount(count.value, step.value);
+  outputP.classList.remove('hidden');
+});
+
+// hide outputP when an input is focused
+inputs.forEach(function (input) {
+  input.addEventListener('focus', function () {
+    outputP.classList.add('hidden');
+  });
+});
